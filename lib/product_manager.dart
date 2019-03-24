52994fc6d78f0593259a782febd0abb3c5d66c4d
Razwan Rashidi <razwan.izmi@gmail.com>
2019-03-24 22:50:45 +0800

    Add button to child of product manager

--- lib/product_manager.dart
+++ lib/product_manager.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import './products.dart';
+import './product_control.dart';
 
 class ProductManager extends StatefulWidget {
   final String startingProduct;
@@ -22,21 +23,19 @@ class _ProductManagerState extends State<ProductManager> {
     _products.add(widget.startingProduct);
   }
 
+  void _addProduct(String product) {
+    setState(() {
+      _products.add('Advanced Food Tester');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
         Container(
           margin: EdgeInsets.all(10.0),
-          child: RaisedButton(
-            color: Theme.of(context).primaryColor,
-            child: Text('Add Product'),
-            onPressed: () {
-              setState(() {
-                _products.add('Advanced Food Tester');
-              });
-            },
-          ),
+          child: ProductControl(_addProduct),
         ),
         Products(_products),
       ],
