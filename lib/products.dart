40b9967f1dd1e76fbfe06df60ee7d0fd2fb02c01
Razwan Rashidi <razwan.izmi@gmail.com>
2019-03-26 22:31:41 +0800

    Use ternery operator for conditional rendering

--- lib/products.dart
+++ lib/products.dart
@@ -18,9 +18,13 @@ class Products extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.builder(
-      itemBuilder: _buildProductItem,
-      itemCount: products.length,
-    );
+    return products.length > 0
+        ? ListView.builder(
+            itemBuilder: _buildProductItem,
+            itemCount: products.length,
+          )
+        : Center(
+            child: Text('No products found, please add some.'),
+          );
   }
 }
